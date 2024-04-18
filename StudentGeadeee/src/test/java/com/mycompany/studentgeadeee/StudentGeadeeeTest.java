03c3ecfaf14c5ee479efd655695aa7ee3b9772a1
saleh <xswef87@gmail.com>
2024-04-18 08:17:06 +0300

    This is first file

---
+++ StudentGeadeee/src/test/java/com/mycompany/studentgeadeee/StudentGeadeeeTest.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.studentgeadeee;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author iivoiil
+ */
+public class StudentGeadeeeTest {
+    
+    public StudentGeadeeeTest() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @Test
+    public void testMain() {
+    }
+    
+}
