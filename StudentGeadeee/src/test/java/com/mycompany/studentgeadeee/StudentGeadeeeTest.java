56f3a9b89fde1f8f3101a918acf101b90519c968
saleh <xswef87@gmail.com>
2024-04-18 08:22:21 +0300

    i made some junit testing to test file

--- StudentGeadeee/src/test/java/com/mycompany/studentgeadeee/StudentGeadeeeTest.java
+++ StudentGeadeee/src/test/java/com/mycompany/studentgeadeee/StudentGeadeeeTest.java
@@ -29,5 +29,53 @@ public class StudentGeadeeeTest {
     @Test
     public void testMain() {
     }
+     @Test
+    public void testGetGrade95() {
+        System.out.println("getGrade 95");
+        int mark = 95;
+        String expResult = "A";
+        String result = StudentGeadeee.getGrade(mark);
+        assertEquals(expResult, result);
+    }
+    @Test
+    public void testGetGrade85() {
+        System.out.println("getGrade 85");
+        int mark = 85;
+        String expResult = "B";
+        String result = StudentGeadeee.getGrade(mark);
+        assertEquals(expResult, result);
+    }
+    @Test
+    public void testGetGrade75() {
+        System.out.println("getGrade 85");
+        int mark = 75;
+        String expResult = "F";
+        String result = StudentGeadeee.getGrade(mark);
+        assertEquals(expResult, result);
+    }
+    @Test
+    void testGrade65(){
+        System.out.println("getGrade 65");
+        int mark = 65;
+        String expResult = "F";
+        String Result = StudentGeadeee.getGrade(mark);
+        assertEquals(expResult,Result);
+    }
+     @Test
+    void testGrade56(){
+        System.out.println("getGrade 56");
+        int mark = 56;
+        String expResult = "D";
+        String Result = StudentGeadeee.getGrade(mark);
+        assertEquals(expResult,Result);
+         System.out.println("saleh");
+    }
+     @Test
+    void testGrade100(){
+        System.out.println("getGrade 100");
+        int mark = 100;
+        String expResult = "A+";
+        String Result = StudentGeadeee.getGrade(mark);
+    }
     
 }
